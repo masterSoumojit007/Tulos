b9e225976462799815cacb9b8a8284c6fa20a5f6
Soumojit Banerjee <soumojitbanerjee22@gmail.com>
2025-01-17 21:34:38 +0530

    updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Extending ESLint configurations for Next.js and TypeScript
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // Adding custom rules to disable specific ESLint rules
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",  // Disable unescaped entities rule
+      "react-hooks/exhaustive-deps": "off",  // Disable exhaustive-deps warning
+    },
+  },
 ];
 
 export default eslintConfig;
